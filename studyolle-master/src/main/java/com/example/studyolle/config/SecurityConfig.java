8345ea832ba88c53c804cf51fac6d09481e1a652
KimSujeong <sujeong0619.b@gmail.com>
2024-02-11 21:53:16 +0900

    Spring Security 설정.(성공)

--- studyolle-master/src/main/java/com/example/studyolle/config/SecurityConfig.java
+++ studyolle-master/src/main/java/com/example/studyolle/config/SecurityConfig.java
@@ -1,24 +1,38 @@
 package com.example.studyolle.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import javax.sql.DataSource;
 
 //Spring Security에서 접근 제한하는 요소 접근 가능하도록
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests()
-                .mvcMatchers("/", "/login", "/sign-up", "/check-email", "/check-email-token",
-                        "/email-login", "/check-email-login", "/login-link").permitAll()
-                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
-                .anyRequest().authenticated();
+    @Bean
+    SecurityFilterChain fileterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception{
+        MvcRequestMatcher.Builder mvcMaatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+        http.authorizeHttpRequests((requests)->requests
+                .requestMatchers(
+                        mvcMaatcherBuilder.pattern("/"), mvcMaatcherBuilder.pattern("/login"), mvcMaatcherBuilder.pattern("/sign-up")
+                        ,mvcMaatcherBuilder.pattern("/check-email") ,mvcMaatcherBuilder.pattern("/check-email-token"),
+                        mvcMaatcherBuilder.pattern("/check-email-login"), mvcMaatcherBuilder.pattern("/email-login"), mvcMaatcherBuilder.pattern("/profile/*")
 
+                ).permitAll()
+                .requestMatchers(mvcMaatcherBuilder.pattern(HttpMethod.POST, "/profile/*")).permitAll()
+                .anyRequest().authenticated()
+        );
+
+        return http.build();
     }
-
 }
